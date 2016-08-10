d6ea68e2b2d7c4b233c8b1ee3aad75274e33acaa
romybrunner <romybrunner@hotmail.com>
2016-08-10 14:35:48 -0500

    actual

--- app/src/main/java/com/example/romy/juegopeligroso2/prueba.java
+++
@@ -1,7 +0,0 @@
-package com.example.romy.juegopeligroso2;
-
-/**
- * Created by romy on 08/07/16.
- */
-public class prueba {
-}
--- app/src/main/java/com/example/romy/pruebacomida/MainActivity.java
+++ app/src/main/java/com/example/romy/pruebacomida/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.romy.juegopeligroso2;
+package com.example.romy.pruebacomida;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
